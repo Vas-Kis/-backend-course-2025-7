0a4f0e2307ba8005c348fc6916a654d7ecd9b4c2
Vasyl Kyslianka bc2025-6 <vasyl.kyslianka@lnu.edu.ua>
2025-11-17 22:03:28 +0200

    Modifying main.js, Lord have mercy

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-import http from "http";
-import fs from "fs/promises";
-import { Command } from "commander";
+import express from 'express';
+import * as fs from "fs/promises";
+import { program } from "commander";
 import path from "path";
-import request from 'superagent';
+import { fileURLToPath } from 'url';
+import multer from 'multer';
 
-const program = new Command();
 program
   .requiredOption("-h, --host <host>", "server host")
   .requiredOption("-p, --port <port>", "server port")
   .requiredOption("-c, --cache <path>", "cache directory");
 program.parse(process.argv);
 
-const { host, port, cache } = program.opts();
-const cache_path = path.resolve(cache);
+const options = program.opts();
+
+if (!options.host || !options.port || !options.cache) {
+  console.error('Error : please specify the neccesary input parameters! (host, port and cache folder)');
+  process.exit(1);
+}
 
 await fs.mkdir(cache_path, { recursive: true });
 
-const server = http.createServer(async (req, res) => {
-  const code = req.url.slice(1); 
-  const filePath = path.join(cache_path, `${code}.jpeg`);
-  console.log("Server started");
-  res.writeHead(200);
-  res.end("Welcome to the server");
+const app = express();
+
+let inventory [];
+
+app.get("/inventory", (req, res) => {
+    res.json();
 });
 
-server.listen(port, host, () => {
+app.listen(port, host, () => {
   console.log(`Proxy server running at http://${host}:${port}/`);
 });
